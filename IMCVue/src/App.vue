5acda2b932a81e16ba3cdc5c191e98ac909f2d52
Fabio Didio <120029824+fabiopdidio@users.noreply.github.com>
2023-08-07 11:17:22 -0300

    update app

--- IMCVue/src/App.vue
+++ IMCVue/src/App.vue
@@ -1,13 +1,22 @@
 <template>
   <div>
-    
+    <h1>Calculadora de IMC</h1>
+    <input type="number" placeholder="Altura em metros" />
+    <input type="number" placeholder="Peso em Kg" />
+    <button>Calcular</button>
+    <h2>{{ resultado }}</h2>
   </div>
 </template>
 <script>
 export default {
-  
-}
+  data() {
+    return {
+      altura: 0,
+      peso: 0,
+      resultado: "",
+    };
+  },
+  methods: {},
+};
 </script>
-<style>
-  
-</style>
+<style></style>
