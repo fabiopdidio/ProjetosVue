26eabeb58422c494839d8625bb0664f60744dd83
Fabio Didio <120029824+fabiopdidio@users.noreply.github.com>
2023-08-07 15:56:35 -0300

    update app

--- IMCVue/src/App.vue
+++ IMCVue/src/App.vue
@@ -1,12 +1,13 @@
 <template>
   <div>
     <h1>Calculadora de IMC</h1>
-    <input type="number" placeholder="Altura em metros" />
-    <input type="number" placeholder="Peso em Kg" />
-    <button>Calcular</button>
+    <input type="number" placeholder="Altura em metros" v-model="altura" />
+    <input type="number" placeholder="Peso em Kg" v-model="peso" />
+    <button @click="calcularImc">Calcular</button>
     <h2>{{ resultado }}</h2>
   </div>
 </template>
+
 <script>
 export default {
   data() {
@@ -16,7 +17,27 @@ export default {
       resultado: "",
     };
   },
-  methods: {},
+  methods: {
+    calcularImc() {
+      // Define aviso em caso do usuário não preencher os campos ou digitar 0
+      if (this.peso === 0 || this.altura === 0) {
+        alert("Preencha todos os dados");
+        return;
+      }
+      // Fórmula do IMC
+      let imc = (this.peso / this.altura ** 2).toFixed(2);
+      // Mensagem ao usupario, baseado no resultado do cálculo
+      if (imc < 18.5) {
+        this.resultado = `IMC ${imc} - Abaixo do peso.`;
+      } else if (imc >= 18.5 && imc <= 24.9) {
+        this.resultado = `IMC ${imc} - Peso ideal.`;
+      } else if (imc >= 25 && imc <= 29.9) {
+        this.resultado = `IMC ${imc} - Sobrepeso.`;
+      } else if (imc > 30) {
+        this.resultado = `IMC ${imc} - Obesidade.`;
+      }
+    },
+  },
 };
 </script>
 <style></style>
